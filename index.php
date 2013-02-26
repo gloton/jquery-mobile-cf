72ecffa539cf0c79021394eefc5bd89f6201aa75
Gloton <gloton@gmail.com>
2013-02-26 09:45:33 -0300

    5 Asignar tema Propiedad para poder asignar un tema

--- index.php
+++ index.php
@@ -13,12 +13,27 @@
 <div data-role="page">
 <!-- bajo este datarol contendra toda la aplicacion de nuesto jqm -->
 <!-- cabecera -->
-	<div data-role="header">
+	<div data-role="header" data-theme="c">
 		<h1>Esto es una cabecera</h1>
 	</div>
 <!-- contenido -->
 	<div data-role="content">
-		<p>Esto es un contenido</p>
+		<p>A elementum consectetur faucibus vulputate quisque dui cras dui, vehicula dui at cras viverra 
+		phasellus ut semper, augue maecenas ipsum accumsan phasellus netus fusce at pulvinar fringilla 
+		ligula dictumst phasellus congue donec placerat, porta tincidunt cursus vitae eros porta praesent 
+		senectus, rutrum taciti aliquam mattis libero metus nibh neque litora consectetur aptent inceptos 
+		nulla sapien, rhoncus aliquet scelerisque conubia placerat elementum, sapien porta risus sodales 
+		enim quisque vestibulum iaculis praesent quisque eros enim eleifend, neque eget quisque hac 
+		condimentum congue, amet ullamcorper aliquet etiam metus turpis.
+		</p>
+		<p>A elementum consectetur faucibus vulputate quisque dui cras dui, vehicula dui at cras viverra 
+		phasellus ut semper, augue maecenas ipsum accumsan phasellus netus fusce at pulvinar fringilla 
+		ligula dictumst phasellus congue donec placerat, porta tincidunt cursus vitae eros porta praesent 
+		senectus, rutrum taciti aliquam mattis libero metus nibh neque litora consectetur aptent inceptos 
+		nulla sapien, rhoncus aliquet scelerisque conubia placerat elementum, sapien porta risus sodales 
+		enim quisque vestibulum iaculis praesent quisque eros enim eleifend, neque eget quisque hac 
+		condimentum congue, amet ullamcorper aliquet etiam metus turpis.
+		</p>
 	</div>
 <!-- pie de pagina -->
 	<div data-role="footer"><h2>Esto es un pie de pagina</h2></div>
