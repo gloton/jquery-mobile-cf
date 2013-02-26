e18edff439f9c4a6ee17bffa67b6ce9d721ebed8
Gloton <gloton@gmail.com>
2013-02-26 00:26:39 -0300

    1.2 Agreque jquery data-role video 2 todavia, no se que son los data-roe pero, en este envio es la primera vez que lo aplico y lo pasaron en el video 2, ahora parasare a ver el video 3

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 <html lang="es">
 <head>
 <meta charset="utf-8" />
+<link rel="styleSheet" href="js/jquery.mobile-1.3.0/jquery.mobile-1.3.0.css" type="text/css" media="screen">
+<script type="text/javascript" src="js/jquery-1.8.3.min.js" ></script>
+<script type="text/javascript" src="js/jquery.mobile-1.3.0/jquery.mobile-1.3.0.js" ></script>
 <title></title>
 </head>
 
 <body>
-
+<div data-role="page" data-theme="a">
+</div>
 </body>
 </html>
