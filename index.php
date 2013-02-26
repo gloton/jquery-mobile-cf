0a742761daee1c468ddedb0b5410c3f31fd9134f
Gloton <gloton@gmail.com>
2013-02-26 01:06:19 -0300

    1.3.1 primera version Primera version funcional de una pagina realizada con jquery mobile solo esta realizada utilizando el atributo data-role con distintos valores para la la pagina cabecera contenido y footer

--- index.php
+++ index.php
@@ -5,11 +5,23 @@
 <link rel="styleSheet" href="js/jquery.mobile-1.3.0/jquery.mobile-1.3.0.css" type="text/css" media="screen">
 <script type="text/javascript" src="js/jquery-1.8.3.min.js" ></script>
 <script type="text/javascript" src="js/jquery.mobile-1.3.0/jquery.mobile-1.3.0.js" ></script>
-<title></title>
+<title>Curso de jQuery Mobile</title>
 </head>
 
 <body>
-<div data-role="page" data-theme="a">
+<!-- primera pagina -->
+<div data-role="page">
+<!-- bajo este datarol contendra toda la aplicacion de nuesto jqm -->
+<!-- cabecera -->
+	<div data-role="header">
+		<h1>Esto es una cabecera</h1>
+	</div>
+<!-- contenido -->
+	<div data-role="content">
+		<p>Esto es un contenido</p>
+	</div>
+<!-- pie de pagina -->
+	<div data-role="footer"><h2>Esto es un pie de pagina</h2></div>
 </div>
 </body>
 </html>
